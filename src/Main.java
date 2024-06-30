89d1aa7b84d43833cd938ff458add68ffda43652
MartaD <mmdolinskaya@gmail.com>
2024-06-30 22:52:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+       int startBalance = 250;
+       int refill = 1370;
+
+       int total;
+       if (refill > 1000) {
+           int bonus = refill/100;
+           total = startBalance + refill + bonus;
+       } else {
+           total = startBalance + refill;
+       }
+       System.out.println(total);
+
+    }
+}
